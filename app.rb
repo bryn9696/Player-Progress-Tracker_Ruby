c552d71ee88877c4472fd2898f732fd2d99bd2be
bryn9696 <bryn2009@live.co.uk>
2022-05-21 13:55:51 +0100

    simple log in and sign up functionality

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'sinatra/reloader'
 require './database_connection_setup'
 # require './lib/bookings'
 # require_relative './lib/makersbnb'
-# require './lib/user'
+require './lib/user'
 # require_relative './lib/update'
 # require './lib/images'
 
@@ -22,7 +22,43 @@ class PlayerProgressTracker < Sinatra::Base
   register Sinatra::Flash
 
   get '/' do
-    erb :index
+    erb :login
   end
 
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  post '/sign_up_details' do
+    if User.unique_username(params[:username]) == true
+      flash[:notice] = 'Thanks for signing up to Player progress Tracker'
+      User.create_user(params[:username], params[:password], params[:email], params[:phone_number])
+      redirect '/login'
+    else
+      flash[:notice] = 'This username has been taken - please choose a different one!'
+      redirect '/sign_up'
+    end
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  post '/user_details' do
+    session[:username] = params[:username]
+    session[:password] = params[:password]
+    redirect '/home'
+  end
+
+  get '/home' do
+    if User.valid(session[:username], session[:password])
+      session[:user_id] = User.user_id(username: session[:username])
+      @username = session[:username]
+      erb :home
+    else
+      flash[:notice] = 'Invalid username or password'
+      redirect '/login'
+    end
+    
+  end
 end
