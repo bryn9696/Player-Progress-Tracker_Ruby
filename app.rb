1c9a164552371c7a432f513fbd65c5698e6591b6
bryn9696 <bryn2009@live.co.uk>
2022-05-21 11:27:56 +0100

    basic set up

---
+++ app.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'sinatra/flash'
+require 'sinatra/reloader'
+# require './database_connection_setup'
+# require './lib/bookings'
+# require_relative './lib/makersbnb'
+# require './lib/user'
+# require_relative './lib/update'
+# require './lib/images'
+
+
+# App class
+class PlayerProgressTracker < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  enable :sessions
+  register Sinatra::Flash
+
+  get '/' do
+    erb :index
+  end
+
+end
