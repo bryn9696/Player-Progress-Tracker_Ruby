96ded61b23b7a0409d68d4fc74d23ecad9036e59
bryn9696 <bryn2009@live.co.uk>
2022-05-22 16:25:20 +0100

    all team spec tests pas s

--- spec/teams_spec.rb
+++ spec/teams_spec.rb
@@ -16,11 +16,11 @@ describe MyTeams do
     expect(MyTeams.exist?(team_name: 'Another Team')).to eq true 
   end
 
-#   it '.view_team' do
-#     MyTeams.create_team(team_name: 'Another Team',number_of_players: '1',coach_or_manager: 'bryn', my_teams_id: 1, user_id: 1)
-#     DatabaseConnection.query('SELECT * FROM my_teams')
-#     expect(MyTeams.view_teams[0]['name']).to eq('Another Team')
-#     expect(MyTeams.view_teams[0]['id']).to eq('1')
-#   end
+  it '.view_team' do
+    MyTeams.create_team(team_name: 'Another Team',number_of_players: '1',coach_or_manager: 'bryn', my_teams_id: 1, user_id: 1)
+    DatabaseConnection.query('SELECT * FROM my_teams')
+    expect(MyTeams.view_teams[0]['team_name']).to eq('Another Team')
+    expect(MyTeams.view_teams[0]['id']).to eq('1')
+  end
   
 end
